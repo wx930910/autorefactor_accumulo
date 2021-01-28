e58b0f25e8bbe77cdbeb11d7a9999d7f0e6044eb
Wang Xiao <wx19930910@gmail.com>
2021-01-27 19:49:08 -0500

    auto refactor MockRateLimiter

--- core/src/test/java/org/apache/accumulo/core/file/streams/RateLimitedOutputStreamTest.java
+++ core/src/test/java/org/apache/accumulo/core/file/streams/RateLimitedOutputStreamTest.java
@@ -20,36 +20,48 @@ import static org.junit.Assert.assertEquals;
 
 import java.security.SecureRandom;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
 
+import org.apache.accumulo.core.util.ratelimit.RateLimiter;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import com.google.common.io.ByteStreams;
 import com.google.common.io.CountingOutputStream;
 
 public class RateLimitedOutputStreamTest {
 
-  @Test
-  public void permitsAreProperlyAcquired() throws Exception {
-    Random randGen = new SecureRandom();
-    MockRateLimiter rateLimiter = new MockRateLimiter();
-    long bytesWritten = 0;
-    try (RateLimitedOutputStream os =
-        new RateLimitedOutputStream(new NullOutputStream(), rateLimiter)) {
-      for (int i = 0; i < 100; ++i) {
-        byte[] bytes = new byte[Math.abs(randGen.nextInt() % 65536)];
-        os.write(bytes);
-        bytesWritten += bytes.length;
-      }
-      assertEquals(bytesWritten, os.position());
-    }
-    assertEquals(bytesWritten, rateLimiter.getPermitsAcquired());
-  }
+	@Test
+	public void permitsAreProperlyAcquired() throws Exception {
+		Random randGen = new SecureRandom();
+		RateLimiter rateLimiter = Mockito.mock(RateLimiter.class);
+		AtomicLong rateLimiterPermitsAcquired = new AtomicLong();
+		try {
+			Mockito.doAnswer((stubInvo) -> {
+				long permits = stubInvo.getArgument(0);
+				rateLimiterPermitsAcquired.addAndGet(permits);
+				return null;
+			}).when(rateLimiter).acquire(Mockito.anyLong());
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+		long bytesWritten = 0;
+		try (RateLimitedOutputStream os = new RateLimitedOutputStream(new NullOutputStream(), rateLimiter)) {
+			for (int i = 0; i < 100; ++i) {
+				byte[] bytes = new byte[Math.abs(randGen.nextInt() % 65536)];
+				os.write(bytes);
+				bytesWritten += bytes.length;
+			}
+			assertEquals(bytesWritten, os.position());
+		}
+		assertEquals(bytesWritten, rateLimiterPermitsAcquired.get());
+	}
 
-  public static class NullOutputStream extends FSDataOutputStream {
-    public NullOutputStream() {
-      super(new CountingOutputStream(ByteStreams.nullOutputStream()), null);
-    }
-  }
+	public static class NullOutputStream extends FSDataOutputStream {
+		public NullOutputStream() {
+			super(new CountingOutputStream(ByteStreams.nullOutputStream()), null);
+		}
+	}
 
 }
